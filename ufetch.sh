414e1c44b17620d746272cd060ed6d4bd8221460
David <jdu@ciencias.unam.mx>
2022-09-29 01:55:42 -0500

    Add some stuff

---
+++ ufetch.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+#
+# ufetch-arch - tiny system info for arch
+
+## INFO
+
+# user is already defined
+host="$(cat /etc/hostname)"
+os='Arch Linux'
+kernel="$(uname -sr)"
+uptime="$(uptime -p | sed 's/up //')"
+packages="$(pacman -Q | wc -l)"
+shell="$(basename "${SHELL}")"
+
+## UI DETECTION
+
+parse_rcs() {
+	for f in "${@}"; do
+		wm="$(tail -n 1 "${f}" 2> /dev/null | cut -d ' ' -f 2)"
+		[ -n "${wm}" ] && echo "${wm}" && return
+	done
+}
+
+rcwm="$(parse_rcs "${HOME}/.xinitrc" "${HOME}/.xsession")"
+
+ui='unknown'
+uitype='UI'
+if [ -n "${DE}" ]; then
+	ui="${DE}"
+	uitype='DE'
+elif [ -n "${WM}" ]; then
+	ui="${WM}"
+	uitype='WM'
+elif [ -n "${XDG_CURRENT_DESKTOP}" ]; then
+	ui="${XDG_CURRENT_DESKTOP}"
+	uitype='DE'
+elif [ -n "${DESKTOP_SESSION}" ]; then
+	ui="${DESKTOP_SESSION}"
+	uitype='DE'
+elif [ -n "${rcwm}" ]; then
+	ui="${rcwm}"
+	uitype='WM'
+elif [ -n "${XDG_SESSION_TYPE}" ]; then
+	ui="${XDG_SESSION_TYPE}"
+fi
+
+ui="$(basename "${ui}")"
+
+## DEFINE COLORS
+
+# probably don't change these
+if [ -x "$(command -v tput)" ]; then
+	bold="$(tput bold)"
+	black="$(tput setaf 0)"
+	red="$(tput setaf 1)"
+	green="$(tput setaf 2)"
+	yellow="$(tput setaf 3)"
+	blue="$(tput setaf 4)"
+	magenta="$(tput setaf 5)"
+	cyan="$(tput setaf 6)"
+	white="$(tput setaf 7)"
+	reset="$(tput sgr0)"
+fi
+
+# you can change these
+lc="${reset}${bold}${blue}"         # labels
+nc="${reset}${bold}${green}"         # user and hostname
+ic="${reset}${magenta}"                       # info
+c0="${reset}${white}"                # first color
+c1="${reset}${cyan}"
+
+## Default OUTPUT
+
+# ${c1}        /\\         ${nc}${USER}${ic}@${nc}${host}${reset}
+# ${c1}       /  \\        ${lc}OS:        ${ic}${os}${reset}
+# ${c1}      /\\   \\       ${lc}KERNEL:    ${ic}${kernel}${reset}
+# ${c0}     /  __  \\      ${lc}UPTIME:    ${ic}${uptime}${reset}
+# ${c0}    /  (  )  \\     ${lc}PACKAGES:  ${ic}${packages}${reset}
+# ${c0}   / __|  |__\\\\    ${lc}SHELL:     ${ic}${shell}${reset}
+# ${c0}  /.\`        \`.\\   ${lc}${uitype}:        ${ic}${ui}${reset}
+
+## OUTPUT
+cat <<EOF
+
+${c1}       /\\	 ${nc}${USER}${ic}@${nc}${host}${reset}
+${c1}      /  \\	 ${lc}os:     ${ic}${os}${reset}	
+${c1}     /\\   \\	 ${lc}kernel: ${ic}${kernel}${reset}
+${c0}    /      \\	 ${lc}uptime: ${ic}${uptime}${reset}
+${c0}   /   ,,   \\	 ${lc}pkgs:   ${ic}${packages}${reset}
+${c0}  /   |  |  -\\	 ${lc}shell:  ${ic}${shell}${reset}
+${c0} /_-''    ''-_\\	 ${lc}${uitype}:     ${ic}${ui}${reset}
+
+EOF
