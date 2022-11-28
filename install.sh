4401fcf80d2f7c1655eb2141c723ac97f1b53188
David <jdu@ciencias.unam.mx>
2022-11-28 00:24:08 -0600

    Add dependency

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ cd paru; makepkg -si;cd
 cp --parents -r .local/bin ~
 cp -r .config ~
 mkdir -p ~/Media/Images/wallpapers
+mkdir ~/Media/Images/screenshots
 cp wallpaper.png ~/Pictures/wallpapers/
 cp .xinitrc ~
 cp .zshrc ~
@@ -20,7 +21,7 @@ paru -S bspwm sxhkd alacritty rofi polybar ranger ueberzug \
 	zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting \
 	picom-ibhagwan-git ttf-material-design-icons-desktop-git \
 	nerd-fonts-fantasque-sans-mono firefox doas man htop \
-	xorg-xsetroot hsetroot dunst sxiv libnotify escrotum-git
+	xorg-xsetroot hsetroot dunst sxiv libnotify escrotum-git polkit
 
 
 echo "permit ${USER} as root" | sudo tee /etc/doas.conf
@@ -30,5 +31,3 @@ chsh -s /usr/bin/zsh
 
 echo "Installing starship prompt"
 curl -sS https://starship.rs/install.sh | sh
-
-mkdir 
