f96f6b4d2b01c5acce71aa30890d39261319a2fe
David <jdu@ciencias.unam.mx>
2022-12-16 17:50:51 -0600

    Fix install script

--- install.sh
+++ install.sh
@@ -5,9 +5,18 @@ sudo pacman -Syu
 
 echo "Installing paru"
 git clone https://aur.archlinux.org/paru.git
-cd paru; makepkg -si;cd
+cd paru && makepkg -si && cd ..
 
+# Make a backup of previous config(if there is one)
+mkdir ~/config_backup
+cp -r ~/.config ~/config_backup
+cp ~/.xinitrc ~/config_backup
+cp ~/.zshrc ~/config_backup
+cp ~/.Xresources ~/config_backup
+
+# Copy config files
 cp --parents -r .local/bin ~
+cp --parents -r .local/share/icons/dunst ~
 cp -r .config ~
 mkdir -p ~/Media/Images/wallpapers
 mkdir ~/Media/Images/screenshots
@@ -23,7 +32,7 @@ paru -S bspwm sxhkd alacritty rofi polybar ranger ueberzug \
 	nerd-fonts-fantasque-sans-mono firefox doas man htop \
 	xorg-xsetroot hsetroot dunst sxiv libnotify escrotum-git polkit
 
-
+# Give root permisions with doas
 echo "permit ${USER} as root" | sudo tee /etc/doas.conf
 
 echo "Setting zsh as default shell"
