fac36a27b5c40aead5bbe48b6d6c178c47079f10
David <jdu@ciencias.unam.mx>
2024-03-16 00:32:49 -0600

    Delete more old configs no longer used

--- .config/polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-nate already running bar instances
-killall -q polybar
-# If all your bars have ipc enabled, you can also use
-# polybar-msg cmd quit
-
-# Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar mybar_m 2>&1 | tee -a /tmp/polybar.log & disown
-
-echo "Polybar launched..."
